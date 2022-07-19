e35bbc83fb509bde5dab56e0069354895efe6025
Kubanychbek Alymbekov <alymbekovkubanychbek@gmail.com>
2022-07-19 08:10:18 +0600

    sdf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,40 @@
+package com.company;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        run();
+    }
+
+    public static void run() {
+//        var contain = new HashMap<Integer, String>();
+//        contain.put(100,"Бананочипсы");
+//        contain.put(30,"Конфета ");
+//        contain.put(25,"Вода");
+
+      //  System.out.println(contain);
+      // Machine machine = new Machine();
+//       List<Machine> machineList = new ArrayList<>();
+//        machineList.add(new Machine(100,"Бананочипсы"));
+//        machineList.add(new Machine(30,"Конфета"));
+//        machineList.add(new Machine(25,"Вода"));
+//        Machine machine = new Machine();
+//        machine.setName(machineList.get(1).toString());
+//        System.out.println(machine.getName());
+//
+//       System.out.println(machineList);
+        List<Machine> machineList = new ArrayList<>();
+        Machine machine = new Machine();
+        machine.makeMachine(machineList);
+
+
+
+
+
+
+
     }
 }
